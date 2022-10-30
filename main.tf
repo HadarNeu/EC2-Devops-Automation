f5eaa18c8d1784a4efb004008ca29d9e6dcf0cce
Hadar Neulander <110464917+HadarNeu@users.noreply.github.com>
2022-10-30 13:13:13 +0200

    keys problem solved.
    The Ansible file is running successfully using the Terraform files. 
    Work continues...

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_security_group" "allow_tcp" {
 resource "aws_instance" "WebHost" {
     ami = var.ami
     instance_type = var.instance_type
-    key_name = var.key_name
+    key_name = "Terraform_test1"
     vpc_security_group_ids = ["${aws_security_group.allow_tcp.id}"]
     subnet_id = aws_subnet.public-subnet.id
     associate_public_ip_address = true
